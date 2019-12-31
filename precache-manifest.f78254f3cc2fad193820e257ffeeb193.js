75b171f5dd2e696513ce541d370577f8abb1bf3b
Hadi <hadi.assalem@gmail.com>
2019-12-31 13:59:30 +0100

    Updates

---
+++ precache-manifest.f78254f3cc2fad193820e257ffeeb193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081f4038afed5fbdf56651876e43ce97",
+    "url": "/Monsters-Land/index.html"
+  },
+  {
+    "revision": "34437fb2accc23f6a4c2",
+    "url": "/Monsters-Land/static/css/main.c720737d.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monsters-Land/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "34437fb2accc23f6a4c2",
+    "url": "/Monsters-Land/static/js/main.2ab3baa3.chunk.js"
+  },
+  {
+    "revision": "751f59c8574e21694eb6",
+    "url": "/Monsters-Land/static/js/runtime-main.f167d5ee.js"
+  }
+]);
